ef651a60a465b1a134bd93491e72ac74b8ad3463
Oleksandr Basiuk <basuksasa@gmail.com>
2023-07-06 17:58:56 +0200

    update 06.07.2023

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import styled from "styled-components";
 import './styles/App.css';
 import NavBar from "./components/NavBar/NavBar";
 import Main from "./components/Main/Main";
@@ -9,12 +8,6 @@ import Artists from "./components/Artists/Artists";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
 
-const AppWrapper = () => styled.div`
-background-image: url('/public/img/BgIMage.png');
-background-repeat: no-repeat;
-background-size: cover;
-position: relative;
-`
 
 function App() {
   return (
